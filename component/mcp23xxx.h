e5705c35993f670c3e31316749224e87bcdf6d71
Lukas Runge <login@lukasrunge.de>
2021-08-18 20:54:18 +0200

    🎉 initialized repo with boilerplate

---
+++ component/mcp23xxx.h
@@ -0,0 +1,16 @@
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MCP23XXX_H__
+#define __MCP23XXX_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// put your code here ↓↓↓
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MCP23XXX_H__ */
