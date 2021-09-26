550c9af4006b0419d7b0443b256d2576899bba73
Lukas Runge <login@lukasrunge.de>
2021-09-26 14:52:08 +0200

    ✨ now using local version of registers as ssot

--- mcp23xxx.cpp
+++ mcp23xxx.cpp
@@ -1,6 +1,17 @@
 #include "mcp23xxx.h"
 
-mcp23xxx::mcp23xxx(spi_api* spi_handle, uint8_t hardware_address) : spiHandle(*spi_handle), hardware_addr(hardware_address) {
+// define local registers to prevent reading error from causing serious damage
+// this does not affect the low level reading and writing register functions
+#define LOCALREGISTERS
+
+#ifdef LOCALREGISTERS
+// initialize the registers locally to default values
+uint8_t iodir = 0b00000000;
+uint8_t gpio = 0b00000000;
+#endif
+
+mcp23xxx::mcp23xxx(spi_api *spi_handle, uint8_t hardware_address) : spiHandle(*spi_handle), hardware_addr(hardware_address)
+{
     // set HAEN bit to enable hardware addressing
     writeRegister(MCP23XXX_IOCON, readRegister(MCP23XXX_IOCON) | 1 << 3);
 }
@@ -24,29 +35,52 @@ uint8_t mcp23xxx::readRegister(uint8_t register_addr)
     return read;
 }
 
+void mcp23xxx::pinMode(uint8_t pin, bool mode)
+{
 
-void mcp23xxx::pinMode(uint8_t pin, bool mode) {
-    uint8_t iodir = readRegister(MCP23XXX_IODIR);
-    if (mode) {
+#ifndef LOCALREGISTERS
+    // read the current register value
+    iodir = readRegister(MCP23XXX_IODIR);
+#endif
+
+    if (mode)
+    {
         iodir |= (1 << pin);
-    } else {
+    }
+    else
+    {
         iodir &= ~(1 << pin);
     }
     writeRegister(MCP23XXX_IODIR, iodir);
 }
 
-void mcp23xxx::writePin(uint8_t pin, bool value) {
-    uint8_t gpio = readRegister(MCP23XXX_GPIO);
-    if (value) {
+void mcp23xxx::writePin(uint8_t pin, bool value)
+{
+
+#ifndef LOCALREGISTERS
+    // read the current register value
+    gpio = readRegister(MCP23XXX_GPIO);
+#endif
+
+    if (value)
+    {
         gpio |= (1 << pin);
-    } else {
+    }
+    else
+    {
         gpio &= ~(1 << pin);
     }
     writeRegister(MCP23XXX_GPIO, gpio);
 }
 
-void mcp23xxx::togglePin(uint8_t pin) {
-    uint8_t gpio = readRegister(MCP23XXX_GPIO);
+void mcp23xxx::togglePin(uint8_t pin)
+{
+
+#ifndef LOCALREGISTERS
+    // read the current register value
+    gpio = readRegister(MCP23XXX_GPIO);
+#endif
+
     gpio ^= (1 << pin);
     writeRegister(MCP23XXX_GPIO, gpio);
 }
